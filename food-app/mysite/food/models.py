c30387679aca7fed79774f8d077b48edcca3f0eb
Nati Melkamu <natimelkemu@gmail.com>
2025-07-16 10:14:41 -0700

    with css for decor

--- food-app/mysite/food/models.py
+++ food-app/mysite/food/models.py
@@ -10,3 +10,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.TextField()
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
+    item_image = models.CharField(max_length=500, default='https://static.vecteezy.com/system/resources/previews/003/170/825/original/isolated-food-plate-fork-and-spoon-design-free-vector.jpg')
